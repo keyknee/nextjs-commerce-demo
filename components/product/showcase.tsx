2d43638eacded8b70d2ce47f44a94bfdb886ea5a
Toni Kini Foster <tdotstorm@gmail.com>
2024-12-08 20:55:42 -0500

    suspense boundary for product showcase

--- components/product/showcase.tsx
+++ components/product/showcase.tsx
@@ -17,27 +17,31 @@ export async function ProductShowcase(props: Props) {
   // const { url, width, height, altText: alt } = product?.featuredImage || {};
 
   return (
-    <ProductProvider>
-      <div className="flex flex-col items-center gap-4 rounded-lg border border-neutral-200 bg-white p-8 md:p-12 lg:flex-row lg:gap-8 dark:border-neutral-800 dark:bg-black">
-        <div className="h-full w-full max-w-[425px] basis-full lg:basis-4/6">
-          <Suspense
-            fallback={
-              <div className="relative aspect-square h-full max-h-[550px] w-full overflow-hidden" />
-            }
-          >
-            <Gallery
-              images={product!.images.slice(0, 5).map((image: Image) => ({
-                src: image.url,
-                altText: image.altText
-              }))}
-            />
-          </Suspense>
-        </div>
+    <Suspense>
+      <ProductProvider>
+        <div className="flex flex-col items-center gap-4 rounded-lg border border-neutral-200 bg-white p-8 md:p-12 lg:flex-row lg:gap-8 dark:border-neutral-800 dark:bg-black">
+          <div className="h-full w-full max-w-[425px] basis-full lg:basis-4/6">
+            <Suspense
+              fallback={
+                <div className="relative aspect-square h-full max-h-[550px] w-full overflow-hidden" />
+              }
+            >
+              <Gallery
+                images={product!.images.slice(0, 5).map((image: Image) => ({
+                  src: image.url,
+                  altText: image.altText
+                }))}
+              />
+            </Suspense>
+          </div>
 
-        <div className="basis-full lg:basis-2/6">
-          <Suspense fallback={null}>{product && <ProductDescription product={product} />}</Suspense>
+          <div className="basis-full lg:basis-2/6">
+            <Suspense fallback={null}>
+              {product && <ProductDescription product={product} />}
+            </Suspense>
+          </div>
         </div>
-      </div>
-    </ProductProvider>
+      </ProductProvider>
+    </Suspense>
   );
 }
